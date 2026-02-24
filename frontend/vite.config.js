42540d21588b735be67c5a45f6b7809dda517882
nullp01nter <123393892+nad-15@users.noreply.github.com>
2026-02-24 21:11:35 +0800

    PWA feature added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -9,5 +9,27 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Movie List',
+        short_name: 'Movie List',
+        description: 'Modern UI/UX Landing Page',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
