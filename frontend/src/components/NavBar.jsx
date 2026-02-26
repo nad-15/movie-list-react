ef8b723252aaa3b50f2b07fddb1ab098908dc963
nad-15 <123393892+nad-15@users.noreply.github.com>
2026-02-26 20:21:32 +0800

    ROUTING fixed

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,11 +4,11 @@ import "../css/Navbar.css"
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/movie-list-react">Movie App</Link>
         </div>
         <div className="navbar-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favorites" className="nav-link">Favorites</Link>
+            <Link to="/movie-list-react" className="nav-link">Home</Link>
+            <Link to="/movie-list-react/favorites" className="nav-link">Favorites</Link>
         </div>
     </nav>
 }
