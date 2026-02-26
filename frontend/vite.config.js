8a374e2d735829ff7763f55e8e7f4139694eb7e3
nad-15 <123393892+nad-15@users.noreply.github.com>
2026-02-26 21:58:00 +0800

    LOGO changed to porper movie list logo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'vite.svg',
+            src: 'movielist.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'vite.svg',
+            src: 'movielist.png',
             sizes: '512x512',
             type: 'image/png'
           }
